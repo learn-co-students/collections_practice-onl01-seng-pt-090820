04091aa216058f1739217a4f6b6af9e76472129c
DavidRMorphew <morphewd@umich.edu>
2020-09-15 20:21:51 -0400

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -14,3 +14,31 @@ def swap_elements(array)
     array[1], array[2] = array [2], array[1]
     array
 end
+
+def swap_elements_from_to(array, index, destination_index)
+    array[index], array[destination_index] = array[destination_index], array[index]
+    array
+end
+
+def reverse_array(array)
+    array = array.reverse
+end
+
+def kesha_maker(array)
+    array.each.map do |word|
+        word.split
+        word[2] = "$"
+        word.to_s 
+    end
+end
+
+def find_a(array)
+    array.select {|word| word.start_with?("a")}
+end
+
+def sum_array(array)
+    sum = 0
+    array.each {|n| sum+=n}
+    sum
+end
+
